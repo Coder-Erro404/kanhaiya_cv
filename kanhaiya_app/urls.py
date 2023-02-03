561ec5b43c6ebda69429b2434dcc7dcdd96de466
Coder-Erro404 <kkhansare@gmail.com>
2023-02-03 17:42:58 +0530

    First commit

--- kanhaiya_app/urls.py
+++ kanhaiya_app/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path
-
-from .views import index
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
-    path('', index),
-]
+    path("",views.index, name='kanhaiya'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
