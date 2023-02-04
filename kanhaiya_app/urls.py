ff91013dd9c17c3a601957abdde7faa2f87123a6
Coder-Erro404 <kkhansare@gmail.com>
2023-02-04 11:20:41 +0530

    First commit

--- kanhaiya_app/urls.py
+++ kanhaiya_app/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path("",views.index, name='kanhaiya'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
