addac3c644b12f19e0415693cc82aede52cf4e4e
Coder-Erro404 <kkhansare@gmail.com>
2023-02-04 12:22:49 +0530

    First commit

--- kanhaiya/settings.py
+++ kanhaiya/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles_build/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
