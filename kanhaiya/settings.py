31d406241368c9c8c132f8bdc861e17adfcde8e0
Coder-Erro404 <kkhansare@gmail.com>
2023-02-04 15:32:17 +0530

    First commit

--- kanhaiya/settings.py
+++ kanhaiya/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'kanhaiya.urls'
@@ -122,6 +123,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- kanhaiya_app/urls.py
+++ kanhaiya_app/urls.py
@@ -8,3 +8,5 @@ from . import views
 urlpatterns = [
     path("",views.index, name='kanhaiya'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
