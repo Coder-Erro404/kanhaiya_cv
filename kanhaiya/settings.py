8c76a84b71615165cfc3340304541d79361d001f
Coder-Erro404 <kkhansare@gmail.com>
2023-02-06 12:05:03 +0530

    First commit

--- kanhaiya/settings.py
+++ kanhaiya/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [ BASE_DIR / 'theme/static' ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
