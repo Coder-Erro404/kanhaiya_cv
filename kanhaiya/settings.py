1e9b3dcbda44d15195ddaa3c1d45ef5b6fc3bf3f
Coder-Erro404 <kkhansare@gmail.com>
2023-02-07 12:22:11 +0530

    First commit

--- kanhaiya/settings.py
+++ kanhaiya/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kanhaiya_app'
     
 ]
 
@@ -126,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
